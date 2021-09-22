713d35aed6aee3a8c5977b9c13f63c0de68f4f77
Brock Herion <brockheriondev@gmail.com>
2021-09-22 13:02:13 -0500

    Added Plausible analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import 'tailwindcss/tailwind.css'
+import PlausibleProvider from 'next-plausible';
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+    <PlausibleProvider domain='https://brockherion.vercel.app/'>
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
