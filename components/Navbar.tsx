d07525069571bbd6184b93c07880b5e5136af2c3
Brock Herion <brockheriondev@gmail.com>
2021-09-23 06:10:54 -0500

    Added Google fonts and tweaked some spacing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,22 +5,22 @@ function Navbar() {
   return (
     <nav>
       <ul className="flex items-center">
-        <li className="text-gray-100 mr-3 hover:underline">
+        <li className="text-gray-100 mr-3 hover:underline font-source-code-pro">
           <Link href="/About">
             <a>About</a>
           </Link>
         </li>
-        <li className="text-gray-100 mr-3 hover:underline">
+        <li className="text-gray-100 mr-3 hover:underline font-source-code-pro">
           <Link href="/articles">
             <a>Articles</a>
           </Link>
         </li>
-        <li className="text-gray-100 mr-3 hover:underline">
+        <li className="text-gray-100 mr-3 hover:underline font-source-code-pro">
           <Link href="/tech">
             <a>Tech</a>
           </Link>
         </li>
-        <li className="text-gray-100 hover:underline">
+        <li className="text-gray-100 hover:underline font-source-code-pro">
           <Link href='newsletter'>
             <a>Newsletter</a>
           </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,53 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col min-h-screen py-2 bg-gray-800">
       <Head>
-        <title>Create Next App</title>
+        <title>Home | Brock Herion</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="grid grid-cols-2 bg-green-500 text-gray-800">
+        <div className="bg-gray-800 2xl:pl-80 xl:pl-72 lg:pl-36 pr-6 md:pl-16 pl-4 py-2 relative" style={{borderRadius: '0 0 80px 0'}}>
+          <Image src='/code.svg' width={500} height={575} />
         </div>
-      </main>
+        <div className="relative">
+          <div className="bg-gray-800 top-0 left-0 absolute" style={{width: '80px', height: '80px'}}></div>
+          <div className="2xl:pr-80 xl:pr-72 lg:pr-36 md:pr-16 pr-6 pl-14 bg-green-500 height-full top-0 left-0 bottom-0 absolute flex flex-col items-end justify-center" style={{borderRadius: '80px 0 0 0'}}>
+            <h1 className="text-4xl mb-3 text-right font-source-code-pro">Hi 👋 I’m Brock!</h1>
+            <h2 className="text-2xl mb-3 text-right font-source-code-pro">
+              I'm a full-stack developer and content creator
+            </h2>
+            <h3 className="text-lg mb-3 text-right font-open-sans">
+              On this site, you'll find content relating to the software development, along with other areas of a developers life, like health and productivity
+            </h3>
+            <h3 className="text-lg mb-3 text-right font-open-sans">
+              In <i>The Weekly Retrospective</i>, my weekly newsletter, we reflect on the week and share other helpful and interesting content. Enter your email below and join a friendly, growing community of readers!
+            </h3>
+            <form>
+              <input className='bg-gray-100 px-4 py-2 rounded-xl mr-2' type='email' placeholder='Email' />
+              <button className='bg-blue-500 px-4 py-2 rounded-xl text-gray-100' type='submit'>Subscribe</button>
+            </form>
+          </div>
+        </div>
+      </div>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <div className="bg-green-500 pb-2 pt-6 2xl:ml-80 xl:ml-72 lg:ml-36 md:ml-16 ml-6 2xl:pr-80 xl:pr-72 lg:pr-36 md:pr-16 pr-6" style={{borderTopLeftRadius: '80px', borderBottomLeftRadius: '80px'}} >
+        <div className="ml-6">
+          <h2 className="text-2xl mb-3 font-source-code-pro">Check out these recent articles 📰</h2>
+        </div>
+        <div className="grid grid-cols-2 ml-6">
+          <div>
+            <h3 className="text-xl mb-3 font-source-code-pro">Popular Catagories</h3>
+          </div>
+          <div>
+            <h3 className="text-xl mb-3 font-source-code-pro">Featured Articles</h3>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
