72e80a540dd7e8e75323664b523f9769ca3cc64d
Brock Herion <brockheriondev@gmail.com>
2021-09-26 15:04:47 -0500

    fixed green on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="grid grid-cols-2 bg-green-600 text-gray-900">
+      <div className="grid grid-cols-2 bg-green-500 text-gray-900">
         <div
           className="bg-gray-800 2xl:pl-96 xl:pl-72 lg:pl-36 pr-6 md:pl-16 pl-4 py-2 relative"
           style={{ borderRadius: "0 0 80px 0" }}
@@ -22,7 +22,7 @@ export default function Home() {
             style={{ width: "80px", height: "80px" }}
           ></div>
           <div
-            className="2xl:pr-96 xl:pr-72 lg:pr-36 md:pr-16 pr-6 pl-14 pt-6 bg-green-600 height-full top-0 left-0 absolute flex flex-col items-end justify-center"
+            className="2xl:pr-96 xl:pr-72 lg:pr-36 md:pr-16 pr-6 pl-14 pt-6 bg-green-500 height-full top-0 left-0 absolute flex flex-col items-end justify-center"
             style={{ borderRadius: "80px 0 0 0" }}
           >
             <h1 className="2xl:text-5xl xl:text-4xl text-3xl mb-3 text-right font-source-code-pro">
@@ -62,7 +62,7 @@ export default function Home() {
       </div>
 
       <div
-        className="bg-green-600 text-gray-800 pb-2 pt-6 2xl:ml-96 xl:ml-72 lg:ml-36 md:ml-16 ml-6 2xl:pr-96 xl:pr-72 lg:pr-36 md:pr-16 pr-6"
+        className="bg-green-500 text-gray-800 pb-2 pt-6 2xl:ml-96 xl:ml-72 lg:ml-36 md:ml-16 ml-6 2xl:pr-96 xl:pr-72 lg:pr-36 md:pr-16 pr-6"
         style={{ borderTopLeftRadius: "80px", borderBottomLeftRadius: "80px" }}
       >
         <div className="ml-6">
