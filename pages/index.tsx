385a5dfc980dca9a8976e06318b29140e70ae2cc
Brock Herion <brockheriondev@gmail.com>
2021-10-13 07:43:29 -0500

    Fixed image and curve positioning

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="grid grid-cols-2 justify-items-stretch bg-green-400 text-gray-900 mb-6">
+      <div className="grid grid-cols-2 justify-items-stretch bg-green-400 text-gray-900">
         <div
           className="bg-gray-800 2xl:pl-96 xl:pl-72 lg:pl-36 pr-6 md:pl-16 pl-4 py-2"
           style={{ borderRadius: "0 0 80px 0" }}
         >
-          <Image src="/code.svg" layout='fill' />
+          <Image src="/code.svg" width={700} height={850} />
         </div>
         <div className="relative">
           <div
@@ -23,7 +23,7 @@ export default function Home() {
             style={{ width: "80px", height: "80px" }}
           ></div>
           <div
-            className="2xl:pr-96 xl:pr-72 lg:pr-36 md:pr-16 pr-6 pl-14 pt-6 bg-green-400 height-full top-0 left-0 absolute flex flex-col items-end justify-center"
+            className="2xl:pr-96 xl:pr-72 lg:pr-36 md:pr-16 pr-6 pl-14 pt-6 bg-green-400 height-full top-0 left-0 relative flex flex-col items-end justify-center"
             style={{ borderRadius: "80px 0 0 0" }}
           >
             <h1 className="2xl:text-5xl xl:text-4xl text-3xl mb-3 text-right font-source-code-pro">
@@ -51,7 +51,7 @@ export default function Home() {
       </div>
 
       <div
-        className="bg-green-400 text-gray-800 pb-2 pt-6 2xl:ml-96 xl:ml-72 lg:ml-36 md:ml-16 ml-6 2xl:pr-96 xl:pr-72 lg:pr-36 md:pr-16 pr-6 relative"
+        className="bg-green-400 text-gray-800 pb-2 pt-6 2xl:ml-96 xl:ml-72 lg:ml-36 md:ml-16 ml-6 2xl:pr-96 xl:pr-72 lg:pr-36 md:pr-16 pr-6"
         style={{ borderTopLeftRadius: "80px", borderBottomLeftRadius: "80px" }}
       >
         <div className="ml-6">
