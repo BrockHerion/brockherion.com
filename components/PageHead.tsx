b56af8caa1abea1789e73d0456b9aba1ae64a657
Brock Herion <brockheriondev@gmail.com>
2022-02-28 18:37:06 -0600

    Fixed URL

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -17,7 +17,7 @@ export default function PageHead({
   const meta = {
     title,
     description,
-    imageUrl: "/default-og-image.png",
+    imageUrl: imageUrl ? imageUrl : "/default-og-image.png",
     siteName: "Brock Herion",
     type: "article",
     twitterHandle: "https://twitter.com/brockherion",
