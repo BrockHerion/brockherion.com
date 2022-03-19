60c275425cff3a7e2044b118a1ff73da3f6374ac
Brock Herion <brockheriondev@gmail.com>
2022-03-19 13:50:28 -0500

    Fixed build error

--- components/blog/BlogHeader.tsx
+++ components/blog/BlogHeader.tsx
@@ -21,13 +21,15 @@ export function BlogPostHeading({
         <div className="w-16 h-2 mt-4 bg-emerald-400"></div>
       </h1>
       <h2 className="md:text-2xl text-xl mb-4 opacity-60">{subTitle}</h2>
-      <Image
-        src={imageSrc ?? ""}
-        width={1600}
-        height={840}
-        alt={title}
-        className="rounded-md"
-      />
+      {imageSrc ? (
+        <Image
+          src={imageSrc}
+          width={1600}
+          height={840}
+          alt={title}
+          className="rounded-md"
+        />
+      ) : null}
       <span className="italic">Published {formatPublishDate(publishedOn)}</span>
     </>
   );
