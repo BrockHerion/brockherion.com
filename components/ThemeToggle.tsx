e2623441a2de6c280cd7fffb20d4316b9071c19f
Brock Herion <brockheriondev@gmail.com>
2022-05-12 11:45:47 -0500

    Added new post

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,8 +1,16 @@
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { IoMoonSharp, IoSunnySharp } from "react-icons/io5";
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true));
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
